39d822c375fceba26cd41756435e616e5d556aea
Unknown <davron.tokhirov@gmail.com>
2020-04-11 00:43:30 +0500

    template

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ class SecondScreen(Screen):
     pass
 
 GUI = Builder.load_file("aukcioner.kv")
-tr = NoTransition()
+
 class MainApp(App):
     def build(self):
         return GUI
     def change_screen(self, screen_name):
+        tr = NoTransition()
         screen_manager = self.root.ids["screen_manager"]
         screen_manager.transition = tr
         screen_manager.current = screen_name
-
+    def change_text(self, label_text):
+        second_screen = self.root.ids["second_screen"]
+        menu_label = second_screen.ids["menu_label"]
+        menu_label.text = label_text
 
 MainApp().run()
